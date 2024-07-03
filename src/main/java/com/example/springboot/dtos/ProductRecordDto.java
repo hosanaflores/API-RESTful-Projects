6d1d6bd885e4668107b81fd11a84f2c67ac279c2
hosanaflores <hosana.flores@sptech.school>
2024-07-03 16:17:07 -0300

    Crianção do CRUD API-RESTful Projects

---
+++ src/main/java/com/example/springboot/dtos/ProductRecordDto.java
@@ -0,0 +1,10 @@
+package com.example.springboot.dtos;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+    
+}
